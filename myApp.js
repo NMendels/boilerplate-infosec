3bc2f3eacd5c79a1c91b5a4a03db418400ebdc9c
N. Mendels <83187276+NMendels@users.noreply.github.com>
2021-05-02 16:43:43 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,5 @@
 var express = require('express');
 var app = express();
-const helmet = require('helmet');
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'DENY'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
 
 
 
